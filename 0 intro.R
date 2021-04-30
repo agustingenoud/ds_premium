2ae570d7cc03057961b1de61999ecfcbcea5f2e5
apu <agustingenoud@MacBook-Pro.local>
2021-04-30 14:40:06 -0300

    Primer prueba competencia data minning. Levantar Dataset.

---
+++ 0 intro.R
@@ -0,0 +1,20 @@
+
+
+rm(list=ls())
+gc()
+
+library(data.table)
+
+getwd()
+#data <- fread("datasets_2/calibrado_201905.csv")
+data <- fread("datasets_2/paquete_premium_201906_202005.csv")
+
+dim(data)
+names(data)
+View(data)
+summary(data)
+
+min(data[, foto_mes])
+max(data[, foto_mes])
+length(data[, foto_mes])
+
